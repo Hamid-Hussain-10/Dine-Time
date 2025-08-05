8d38a0aef472df3b51db603f44ddd4a2965a5dab
Hamid Hussain <hhdev456@gmail.com>
2025-08-05 18:42:08 +0500

    commit

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,60 @@
+import { Tabs } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
+import { StatusBar, View } from "react-native";
+
+export default function TabLayout() {
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar barStyle="light-content"  />
+
+      <Tabs
+        screenOptions={{
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: '#171717',
+          },
+          headerTintColor: '#ffffff',
+          tabBarActiveTintColor: "#ffffff",
+          tabBarInactiveTintColor: "#ffa200",
+          tabBarStyle: {
+            backgroundColor: "#171717",
+            height: 75,
+            paddingBottom: 12,
+          },
+          tabBarLabelStyle: {
+            fontSize: 14,
+            paddingBottom: 5,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="home" color={color} size={24} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="history"
+          options={{
+            title: "History",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="history" color={color} size={24} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="user" color={color} size={24} />
+            ),
+          }}
+        />
+      </Tabs>
+    </View>
+  );
+}
---
+++ app/index.jsx
@@ -0,0 +1,51 @@
+import { router } from "expo-router";
+import {
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
+
+export default function Index() {
+  return (
+    <SafeAreaView className="flex-1 bg-[#2b2b2b]">
+      <StatusBar barStyle="light-content" backgroundColor="#2b2b2b" />
+
+      <View className="flex-1 justify-evenly items-center py-10 px-4">
+        <Image
+          source={require("../assets/images/dinetimelogo.png")}
+          style={{
+            width: 300,
+            height: 150,
+          }}
+        />
+
+        <View className="items-center space-y-4 px-4">
+          <Text className="text-white text-3xl font-bold text-center">
+            Welcome to DineTime
+          </Text>
+          <Text className="text-white text-base text-center">
+            Your perfect companion for managing meals and memories.
+          </Text>
+
+          <TouchableOpacity
+            onPress={() => router.push("/signup")}
+            className="bg-[#f49b33] px-6 py-2 rounded-full shadow-md mt-5 border-2 border-white border-dotted"
+          >
+            <Text className="text-black font-bold text-lg">Next</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Image
+          source={require("../assets/images/Frame.png")}
+          style={{
+            width: 390,
+            height: 90,
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
